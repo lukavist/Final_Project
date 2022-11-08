845b65d41d60d27d91fd2abbafad658f0214bab8
Lance <lancealan.2021@gmail.com>
2022-11-08 17:09:23 -0500

    Added form validation to americano.html and created javascript folder

---
+++ Updated_MidTerm/static_js/products.js
@@ -0,0 +1,51 @@
+
+
+const submit_button = document.getElementById("sub_but");
+// form that submits review
+const reviewForm = document.forms["review"];
+let valid_form = false; 
+submit_button.addEventListener("click", (e) => {
+    console.log("The button was pushed"); 
+
+})
+
+const sub_but = () =>  {
+    console.log("The button is pushed"); 
+}
+
+
+function validateForm() {
+    
+    
+    if (reviewForm.uname.value == "") {
+        return false; 
+        }
+        
+
+        
+
+    else if (reviewForm.email.value == "") {
+        alert("All fields must be filled");
+        return false; 
+    }
+
+    return true; 
+
+
+
+}
+
+reviewForm.addEventListener("submit", (e) => {
+    valid_form = validateForm()
+    const ele = document.getElementsByClassName("field_error")
+    if (!valid_form) {
+        for(let i = 0; i < ele.length; i+=1) {
+            ele[i].innerHTML = "All fields must be full";
+            ele[i].style.backgroundColor = "red"; 
+        }
+        e.preventDefault()
+     }
+})
+
+
+
